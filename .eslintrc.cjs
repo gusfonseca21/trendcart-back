75596980207c6fa284aecd3449919d7244bc3e10
Gustavo Fonseca <gusfonseca.dev@gmail.com>
2023-08-20 00:06:58 -0300

    Base do projeto

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: "",
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
