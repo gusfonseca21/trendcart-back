70d1e6ea3b52764c3cf854c73dd1f3ce4942f3b7
Gustavo Fonseca <gustavo.fonseca@cleversystems.com.br>
2024-01-20 21:13:47 -0300

    Proteção de rotas

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,20 @@
 import { promisify } from "util";
 import { NextFunction, Request, Response } from "express";
-import User from "../models/userModel.js";
+import User, { IUser, Role } from "../models/userModel.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
 import AppError from "../utils/appError.js";
 
+interface AuthRequest extends Request {
+  user?: IUser;
+}
+
 interface NewUserRequest extends Request {
   body: {
+    name: string;
     email: string;
     password: string;
+    passwordConfirm: string;
   };
 }
 
@@ -28,8 +34,10 @@ const signToken = (id: string) => {
 
 export const signup = catchAsync(async (req: NewUserRequest, res: Response) => {
   const newUser = await User.create({
+    name: req.body.name,
     email: req.body.email,
     password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
   });
 
   const token = signToken(newUser._id.toString());
@@ -39,7 +47,9 @@ export const signup = catchAsync(async (req: NewUserRequest, res: Response) => {
     token,
     user: {
       id: newUser._id,
+      name: newUser.name,
       email: newUser.email,
+      photo: newUser.photo,
     },
   });
 });
@@ -60,7 +70,9 @@ export const login = catchAsync(
 
     const loggedUser = {
       id: user._id,
+      name: user.name,
       email: user.email,
+      photo: user.photo,
     };
 
     const token = signToken(user._id.toString());
@@ -69,8 +81,9 @@ export const login = catchAsync(
   }
 );
 
+// protect é usado para vefificar se o usuário está logado
 export const protect = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: AuthRequest, res: Response, next: NextFunction) => {
     // Checar se o token existe
     let token = undefined;
 
@@ -93,10 +106,12 @@ export const protect = catchAsync(
       console.log("Segredo JWT não existe");
     }
 
-    const decoded = await promisify(
-      jwt.verify
-      // @ts-expect-error Diz que jwt.verify espera apenas um argumento em vez de dois
-    )(token, secret);
+    type Decoded = {
+      id: string;
+      iat: string;
+    };
+    //@ts-expect-error Reclamações que não sei como resolver e a aplicação funciona como deveria
+    const decoded: Decoded = await promisify(jwt.verify)(token, secret);
 
     // Checar se o usuário ainda existe
     const user = await User.findById(decoded.id);
@@ -117,7 +132,26 @@ export const protect = catchAsync(
     }
 
     // Acessar a rota protegida
+    // req.user será passado para os próximos middlewares
     req.user = user;
     next();
   }
 );
+
+// restrictTo protege com base na role
+export const restrictTo = (...roles: Role[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    // req.user (informações de usuário) está vindo do middleware de protect
+    if (!req.user)
+      return next(
+        new AppError("O usuário não possui um tipo especificado", 422)
+      );
+
+    if (!roles.includes(req.user.role))
+      return next(
+        new AppError("Você não possui permissão para realizar esta ação", 403)
+      );
+
+    next();
+  };
+};
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,12 +1,16 @@
-import { NextFunction } from "express";
 import { Model, Schema, model } from "mongoose";
 import bcrypt from "bcryptjs";
 import AppError from "../utils/appError.js";
 
+export type Role = "user" | "admin";
+
 export interface IUser extends Document {
+  name: string;
   email: string;
   password: string;
+  passwordConfirm: string | undefined;
   photo: string | null;
+  role: Role;
   createdAt?: Date;
   passwordChangedAt?: Date;
   correctPassword: (
@@ -17,11 +21,16 @@ export interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, "Defina um nome"],
+  },
   email: {
     type: String,
     required: [true, "Defina um email"],
     unique: true,
     lowercase: true,
+    // Validators funcionam somente nos métodos CREATE e SAVE do mongoose
     validate: {
       validator: function (value: string) {
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -36,9 +45,26 @@ const userSchema = new Schema<IUser>({
     minlength: [4, "A senha deve possuir pelo menos quatro caracteres"],
     select: false,
   },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Confirme a senha"],
+    validate: {
+      // Aqui não podemos usar uma arrow function pois utilizaremos o this
+      validator: function (this: IUser, el: string): boolean {
+        return el === this.password;
+      },
+      message: "As senhas não são iguais",
+    },
+  },
   passwordChangedAt: Date,
   photo: {
     type: String,
+    default: null,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
   },
   createdAt: {
     type: Date,
@@ -47,10 +73,12 @@ const userSchema = new Schema<IUser>({
   },
 });
 
-userSchema.pre("save", async function (next: NextFunction) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   try {
     this.password = await bcrypt.hash(this.password, 12);
+    // passwordConfirm serve apenas para validar as senhas inseridas na hora da criação
+    this.passwordConfirm = undefined;
     next();
   } catch (err) {
     next(new AppError("Houve um erro na criptografia da senha", 500));
@@ -64,11 +92,13 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp: number) {
+userSchema.methods.changedPasswordAfter = function (
+  this: IUser,
+  JWTTimestamp: number
+) {
   if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime(JWTTimestamp) / 1000,
-      10
+    const changedTimestamp = Math.floor(
+      this.passwordChangedAt.getTime() / 1000
     );
 
     return JWTTimestamp < changedTimestamp;
